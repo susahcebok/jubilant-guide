660a063de7f2c6f65f2ee7c1208b0aa638e87b04
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-12 15:11:44 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -17,12 +17,9 @@
                 box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
         }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	p {
+                line-height: 1.6;
+        }
 
 	@media (min-width: 640px) {
 		main {
