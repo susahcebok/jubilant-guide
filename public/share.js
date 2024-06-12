cb69f5c5bbdd753ea2afc99fd239146f55e3ea9b
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-12 15:48:34 +0700

    Create share.js

---
+++ public/share.js
@@ -0,0 +1,22 @@
+async function shareCanvas() {
+        const canvas = document.getElementById('cvs');
+        const dataUrl = canvas.toDataURL();
+        const blob = await (await fetch(dataUrl)).blob();
+        const filesArray = [
+    new File(
+      [blob],
+      'prev.png',
+      {
+        type: blob.type,
+        lastModified: new Date().getTime()
+      }
+    )
+  ];
+    const shareData = {
+        files: filesArray,
+        title: "Profile Pic Generator",
+        text: "Bingkai profil Anda dengan warna Palestina. Biarkan foto profil Anda berbicara banyak tentang perdamaian dan keadilan.",
+        url: "https://lonte.netlify.app/",
+  };
+        navigator.share(shareData);
+    }
