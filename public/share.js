7eaa9509e439655d17126df9efe4a190edac1a98
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-12 15:58:08 +0700

    Update share.js

--- public/share.js
+++ public/share.js
@@ -15,7 +15,7 @@ async function shareCanvas() {
     const shareData = {
         files: filesArray,
         title: "Profile Pic Generator",
-        text: "Bingkai profil Anda dengan warna Palestina. Biarkan foto profil Anda berbicara banyak tentang perdamaian dan keadilan.",
+        text: "Bingkai profil Anda dengan warna perlawanan. Biarkan foto profil Anda berbicara banyak tentang perdamaian dan keadilan.",
         url: "https://lonte.netlify.app/",
   };
         navigator.share(shareData);
