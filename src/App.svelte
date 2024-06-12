86bf2adfa1d2a856904cfd60d8f957e501509c7d
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-12 17:10:37 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,29 @@
 </script>
 
 <main>
+        <div class="p f p">
 	<h1>PFP {name}!</h1>
-	<p>Bingkai profil Anda dengan warna perlawanan. Biarkan foto profil Anda berbicara banyak tentang perdamaian dan keadilan.</p>
+        </div>
 
+        <div class="p f p">
+	<p>Bingkai profil Anda dengan warna perlawanan. Biarkan foto profil Anda berbicara banyak tentang perdamaian dan keadilan.</p>
+        </div>
+
+        <div class="p f p">
         <div class="image-upload">
         <label for="file">
         <p><img id="cvs" src="pilihfile.png"/></p>
         </label>
         <input id="file" type="file" accept="image/*" onchange="loadFile(event)" style="display:none;">
         </div>
+        </div>
 
+        <div class="p f p">
         <p>
         <button id="download" onclick="canvasToPng()">Download</button>
         <span id="share"></span>
         </p>
+        </div>
 </main>
 
 <style>
