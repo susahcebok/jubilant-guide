29d6b53876686f83b8ebaa60e2c702c5a81f53cd
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-13 14:30:34 +0700

    Update canvas.js

--- public/canvas.js
+++ public/canvas.js
@@ -57,13 +57,3 @@ image = null;
 canvas = null;
 imgFile = null;
 }
-
-jQuery(document).ready(function($) {
-if (window.history && window.history.pushState) {
-window.history.pushState('forward', null, './#forward');
-$(window).on('popstate', function() {
-alert('Back button was pressed.');
-});
-
-}
-});
