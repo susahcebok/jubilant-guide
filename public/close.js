565e7050b801a62ccb19f2dee218ce8aa3d98308
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2024-06-13 14:50:48 +0700

    Update close.js

--- public/close.js
+++ public/close.js
@@ -1,8 +1,42 @@
-jQuery(document).ready(function($) {
-if (window.history && window.history.pushState) {
-window.history.pushState('forward', null, './#forward');
-$(window).on('popstate', function() {
-alert('Back button was pressed.');
-});
-}
-});
+        // Detect Internet Speed using JavaScript by techandfunzone.in
+//References
+let browserDetailsRef = document.getElementById("browser");
+let osDetailsRef = document.getElementById("os");
+var browserList = [
+  { name: "Firefox", value: "Firefox" },
+  { name: "Opera", value: "OPR" },
+  { name: "Edge", value: "Edg" },
+  { name: "Chrome", value: "Chrome" },
+  { name: "Safari", value: "Safari" },
+];
+var os = [
+  { name: "Android", value: "Android" },
+  { name: "iPhone", value: "iPhone" },
+  { name: "iPad", value: "Mac" },
+  { name: "Macintosh", value: "Mac" },
+  { name: "Linux", value: "Linux" },
+  { name: "Windows", value: "Win" },
+];
+
+let broswerChecker = () => {
+  //Useragent contains browser details and OS  details but we need to separate them
+  let userDetails = navigator.userAgent;
+  for (let i in browserList) {
+    //check if the string contains any value from the array
+    if (userDetails.includes(browserList[i].value)) {
+      //extract browser name and version from the string
+      browserDetailsRef.innerHTML = browserList[i].name || "Unknown Browser";
+      break;
+    }
+  }
+  for (let i in os) {
+    //check if string contains any value from the object
+    if (userDetails.includes(os[i].value)) {
+      //displau name of OS from the object
+      osDetailsRef.innerHTML = os[i].name;
+      break;
+    }
+  }
+};
+
+window.onload = broswerChecker();
